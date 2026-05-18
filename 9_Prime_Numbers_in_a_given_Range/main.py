e67eb0d5ddf1b389a5b228aeb30d92d2fb4bb23b
Anamika <annuanamika.001@gmail.com>
2026-05-19 00:09:14 +0530

    Formatted and added new codes

--- 9_Prime_Numbers_in_a_given_Range/main.py
+++ 9_Prime_Numbers_in_a_given_Range/main.py
@@ -1,11 +1,16 @@
+from math import sqrt
+
 ll = int(input("Enter the lower limit:"))
 ul = int(input("Enter the upper limit:"))
-for n in range(ll,ul):
-    if n >1:
-        for i in range(2,n):
-            if n%i == 0:
-                break
-            else:
-                print(n)
+for n in range(ll, ul):
+    if n > 1:
+        isPrime = True
+        for i in range(2, int(sqrt(n)) + 1):
+            if n % i == 0:
+                isPrime = False
                 break
 
+        if isPrime:
+            print(n)
+    else:
+        print("not a valid input")
